799facc11d3695504c47784e2d823b1001221409
mycelo-dev <karan@mycelo.dev>
2026-04-18 19:06:56 +0530

    Revert "Added option to get_events from db"

--- backend/stream/publish_to_stream.go
+++ backend/stream/publish_to_stream.go
@@ -2,23 +2,22 @@ package stream
 
 import (
 	"context"
-	"encoding/json"
+	"fmt"
 	"time"
 
 	db "github.com/mycelo-dev/mycelo/backend/core"
 	insert_events_queries "github.com/mycelo-dev/mycelo/backend/queries"
 )
 
-func PublishToStream(ctx context.Context, topic string, event_data interface{}) error {
+func PublishToStream(ctx context.Context, topic string, event_data interface{}) {
 
 	sql := insert_events_queries.GetInsertEventsQueries()
+
 	created_at := time.Now().UnixMilli()
 
-	eventBytes, err := json.Marshal(event_data)
-	if err != nil {
-		return err
-	}
+	_, err := db.Get().Exec(ctx, sql, topic, event_data, created_at)
 
-	_, err = db.Get().Exec(ctx, sql, topic, eventBytes, created_at)
-	return err
+	if err != nil {
+		fmt.Println("error inserting data into events table: ", err)
+	}
 }
