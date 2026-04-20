eeefab3836b7b2bd25e738bf7d140c7817199736
mycelo-dev <karan@mycelo.dev>
2026-04-20 11:57:37 +0530

    code add - topics data modelling

---
+++ docs/database/migration/20-apr-2026-topics-data-modelling-01.sql
@@ -0,0 +1,29 @@
+CREATE TABLE TOPICS (
+    topic_id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    topic_public_id UUID DEFAULT GEN_RANDOM_UUID() UNIQUE,
+    tenant_id BIGINT,
+    team_id BIGINT,
+    topic_name VARCHAR(255),
+    created_at BIGINT,
+    updated_at BIGINT
+)
+;
+
+CREATE TABLE TENANTS(
+    tenant_id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    tenant_public_id UUID DEFAULT GEN_RANDOM_UUID() UNIQUE,
+    tenant_name VARCHAR(255),
+    created_at BIGINT,
+    updated_at BIGINT
+)
+;
+
+CREATE TABLE TEAMS(
+    team_id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    team_public_id UUID DEFAULT GEN_RANDOM_UUID() UNIQUE,
+    team_name VARCHAR(255),
+    created_at BIGINT,
+    updated_at BIGINT,
+    tenant_id BIGINT REFERENCES TENANTS(tenant_id)
+)
+;
